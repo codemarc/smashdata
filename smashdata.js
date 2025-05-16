fce6326e2cb3698b73fb73cfc066c54552a1401d
marc j. greenberg <codemarc@gmail.com>
2025-05-15 23:02:27 -0400

    Update version to 1.0.3, enhance import statements, and improve string handling in encryption/decryption functions

--- smashdata.js
+++ smashdata.js
@@ -1,10 +1,10 @@
-import crypto from 'crypto'
+import crypto from 'node:crypto'
 const separator = "::"
 
 export const encrypt = (seed, string) => {
   const key = crypto.createHash("sha256").update(seed).digest("hex").slice(16, 48)
-  let rando = crypto.randomBytes(16)
-  let cipher = crypto.createCipheriv('aes-256-cbc', key, rando)
+  const rando = crypto.randomBytes(16)
+  const cipher = crypto.createCipheriv('aes-256-cbc', key, rando)
   let encryptedString = cipher.update(string)
   encryptedString = Buffer.concat([encryptedString, cipher.final()])
   return rando.toString('hex') + separator + encryptedString.toString('hex')
@@ -13,11 +13,11 @@ export const encrypt = (seed, string) => {
 export const decrypt = (seed, string) => {
   const key = crypto.createHash("sha256").update(seed).digest("hex").slice(16, 48)
   try {
-    let split = string.split(separator)
-    let iv = Buffer.from(split[0], 'hex')
+    const split = string.split(separator)
+    const iv = Buffer.from(split[0], 'hex')
     split.shift()
-    let encryptedText = Buffer.from(split.join(separator), 'hex')
-    let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv)
+    const encryptedText = Buffer.from(split.join(separator), 'hex')
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv)
     let decrypted = decipher.update(encryptedText)
     decrypted = Buffer.concat([decrypted, decipher.final()])
     return decrypted.toString()
